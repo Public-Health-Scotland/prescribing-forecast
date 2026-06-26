da1506e4c44f6cb603899c1e13e34374df30ec78
liam-rooney1 <liam.rooney@phs.scot>
2026-06-26 10:15:08 +0000

    Supplementary charts for final report

--- shiny/setup.R
+++ shiny/setup.R
@@ -41,6 +41,7 @@ library(tibble)
 library(tsibble)
 library(feasts)
 library(GGally)
+library(scales)
 
 # PHS styling packages ----
 library(phsstyles)
@@ -304,6 +305,200 @@ plot <- plot %>%
                 color = "#3F3685"),      # ← Bold effect
     name = "SCOTLAND"
   )
+
+# Monthly trend extraction for Scotland
+timeseries_items_trend <- forecast_items %>%
+  filter(Board == "SCOTLAND") %>%
+  arrange(Date) %>%
+  mutate(
+    month = months(Date),
+    month = factor(month, levels = c(
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ))
+  ) %>%
+  filter(!is.na(Measure)) %>%
+  group_by(month) %>%
+  summarise(Average = mean(Measure), .groups = "drop") %>%
+  arrange(month)
+
+seasonality_items_chart <- ggplot(
+  timeseries_items_trend,
+  aes(x = month, y = Average)) +
+  geom_line(group = 1, color = phs_colours("phs-purple"), linewidth = 1.5) +
+  #geom_point() +
+  scale_y_continuous(labels = comma) +
+  labs(title = "Seasonality chart showing mean number of paid items in Scotland across each month from 2010 to 2025",
+       x = "Month",
+       y = "Mean number of paid items") +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+timeseries_gic_trend <- forecast_gic %>%
+  filter(Board == "SCOTLAND",
+         Date < "2026-01-01") %>%
+  arrange(Date) %>%
+  mutate(
+    month = months(Date),
+    month = factor(month, levels = c(
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ))
+  ) %>%
+  #filter(!is.na(Measure)) %>%
+  group_by(month) %>%
+  summarise(Average = mean(Measure), .groups = "drop") %>%
+  arrange(month)
+
+seasonality_gic_chart <- ggplot(
+  timeseries_gic_trend,
+  aes(x = month, y = Average)) +
+  geom_line(group = 1, color = phs_colours("phs-purple"), linewidth = 1.5) +
+  #geom_point() +
+  scale_y_continuous(labels = comma) +
+  labs(title = "Seasonality chart showing mean Gross Ingredient Cost (£) in Scotland across each month from 2010 to 2025",
+       x = "Month",
+       y = "Mean Gross Ingredient Cost (£)") +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+correlation_items_trend <- forecast_items %>%
+  filter(Board == "SCOTLAND") %>%
+  arrange(Date) %>%
+  filter(Date < "2026-01-01") %>%
+  select(Date, Board, `Number of Paid Items` = Measure)
+
+correlation_gic_trend <- forecast_gic %>%
+  filter(Board == "SCOTLAND") %>%
+  arrange(Date) %>%
+  filter(Date < "2026-01-01") %>%
+  select(Date, Board, `Gross Ingredient Cost (£)` = Measure)
+
+correlation <- left_join(correlation_items_trend, correlation_gic_trend)
+
+correlation_plot <- correlation %>%
+  ggpairs(columns = 3:4,
+          lower = list(continuous = wrap("points", colour = phs_colours("phs-purple"))),
+          diag  = list(continuous = wrap("densityDiag", fill = phs_colours("phs-purple"), colour = phs_colours("phs-purple"))),
+          upper = list(continuous = wrap("cor", colour = phs_colours("phs-purple")))
+  ) +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    strip.text = element_text(size = 14)  # variable names on panels
+  )
+
+
+# Define a scaling factor
+scale_factor <- max(correlation$`Number of Paid Items`, na.rm = TRUE) / 
+  max(correlation$`Gross Ingredient Cost (£)`, na.rm = TRUE)
+
+# pivot_corr <- correlation %>%
+#   pivot_longer(cols = 3:4,
+#                names_to = "Variable",
+#                values_to = "Value")
+
+ggplot(correlation, aes(x = Date, y = `Number of Paid Items`)) +
+  geom_line(colour = phs_colours("phs-purple"), size = 1) +
+  labs(title = "Monthly trend chart showing number of paid items in Scotland from 2010 to 2025",
+       x = "Month",
+       y = "Number of Paid Items") +
+  scale_y_continuous(labels = comma) +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+  
+ggplot(correlation, aes(x = Date, y = `Gross Ingredient Cost (£)`)) +
+  geom_line(colour = phs_colours("phs-purple"), size = 1) +
+  labs(title = "Monthly trend chart showing Gross Ingredient Cost (£) in Scotland from 2010 to 2025",
+       x = "Month",
+       y = "Gross Ingredient Cost (£)") +
+  scale_y_continuous(labels = comma) +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+cpi <- forecast_cpi %>%
+  filter(Board == "SCOTLAND",
+         Date > "2011-03-31" & Date < "2026-01-01") %>%
+  select(Date, Measure) %>%
+  arrange(Date)
+
+ggplot(cpi,
+       aes(x = Date, y = Measure)) +
+  geom_line(colour = phs_colours("phs-purple"), size = 1) +
+  labs(title = "Monthly trend chart showing average cost per item in Scotland from 2010 to 2025",
+       x = "Month",
+       y = "Average cost per item") +
+  scale_y_continuous(labels = comma) +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+acf_pacf_items <- forecast_items %>%
+  filter(Board == "NHS AYRSHIRE & ARRAN") %>%
+  arrange(Date) %>%
+  filter(Date > "2011-01-01" & Date < "2026-01-01") %>%
+  select(Date, `Number of Paid Items` = Measure) %>%
+  mutate(Month = yearmonth(Date)) %>%
+  as_tsibble(index = Month) %>%
+  mutate(
+    diff_1 = difference(`Number of Paid Items`, 1),
+    diff_12 = difference(diff_1, 12)
+  ) %>%
+  gg_tsdisplay(diff_12)
+
+
+autoplot(acf_pacf_items, `Number of Paid Items`)
+
+acf_pacf_items %>%
+  ACF(`Number of Paid Items`) %>%
+  autoplot()
+
+acf_pacf_items %>%
+  gg_tsdisplay(difference(`Number of Paid Items`, 12),
+               plot_type = 'partial', lag = 36) +
+  labs(title="Seasonally differenced", y="")
+
+test_mape <- rbind(`forecast-performance-2026-05-28` %>% select(board, type, MAPE, run), forecast_performance %>% select(board, type, MAPE, run))
+
+t1 <- test_mape %>%
+  filter(!board == "SCOTLAND") %>%
+  group_by(run, type) %>%
+  summarise(avg = mean(MAPE))
+
+##### Read in machine learning model results
+ml_preds <- read_excel("model_outputs.xlsx", sheet = "Predictions") %>%
+  mutate(time = ymd(time))
+
+ml_preds_new <- ml_preds %>%
+  select(Board, Date = time, Type = Target, Measure = Actual, Predicted, Model) %>%
+  pivot_wider(names_from = Model, values_from = Predicted)
+
+forecast_comp <- readRDS(paste0('shiny/forecasts/sarima/output/forecast-run-999.rds')) %>%
+  filter(Historical_Data == "12 months of historical data",
+         Year == 2025,
+         F_Horizon == 48,
+         Arima_Error == FALSE) %>%
+  select(-c(Historical_Data, Year, F_Horizon, Arima_Error))
+
+model_comp <- forecast_comp %>%
+  select(Board, Date, Type, Measure, SARIMA = Forecast) %>%
+  filter(Type %in% c("Claim PD Number of Paid Items", "Claim PD GIC excl. BB")) %>%
+  left_join(ml_preds_new)
+  
 
 ############## Variables ############## 
 healthboards <- unique(forecast_items$Board)
