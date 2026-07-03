5969c4d11b81899d77b2765dbb80125f1f2d8cf0
liam-rooney1 <liam.rooney@phs.scot>
2026-07-03 10:31:26 +0000

    Update code setup to move EDA to relevant server script

--- shiny/pages/eda/eda_server.R
+++ shiny/pages/eda/eda_server.R
@@ -1,7 +1,273 @@
+# ############## EDA Server ######################
 
-# ############## Results Server ######################
+# Putting EDA code in here to remove it from setup file
+eda_data <- read.csv('shiny/forecasts/data/Historical Data.csv', check.names = FALSE) %>%
+  mutate(`Paid Date` = dmy(`Paid Date`)) %>%
+  filter(`Paid Date` > '2009-12-31') 
 
-palette <- phs_colour_values
+eda_data$`Claim PD Paid GIC excl. BB` <- as.double(gsub(",", "", eda_data$`Claim PD Paid GIC excl. BB`))
+
+scotland_data <- eda_data %>%
+  group_by(`Paid Date`) %>%
+  summarise(across(where(is.numeric), ~sum(.x, na.rm = TRUE))) %>%
+  mutate(`Presc Health Board Name` = 'SCOTLAND') %>%
+  select(`Presc Health Board Name`, everything())
+
+eda_data <- eda_data %>%
+  bind_rows(scotland_data) %>%
+  mutate(`Cost per item` = `Claim PD Paid GIC excl. BB` / `Claim PD Number of Paid Items`) %>%
+  # double check data is arranged by board and date 
+  arrange(`Presc Health Board Name`, `Paid Date`)
+
+rm(scotland_data)
+
+# eda_data_tsibble <- eda_data %>%
+#   # create new column for index
+#   mutate(month_new = yearmonth(`Paid Date`)) %>%
+#   tsibble(index = 'month_new')
+
+############## per 1,000 list size (weighted and non-weighted) ############## 
+list_sizes <- read.xlsx('shiny/data/population/List Sizes.xlsx') %>%
+  mutate(quarter_date = as.Date(quarter_date, origin = "1899-12-30")) %>%
+  dplyr::rename(`Paid Date` = quarter_date,
+                `Presc Health Board Name` = Board) %>%
+  select(`Presc Health Board Name`, everything()) %>%
+  left_join(eda_data, by = c('Presc Health Board Name', 'Paid Date')) %>%
+  select(`Presc Health Board Name`, `Paid Date`, `Claim PD Number of Paid Items`, `Claim PD Paid GIC excl. BB`, `Cost per item`, everything()) %>%
+  mutate(
+    Items_1000_LS = `Claim PD Number of Paid Items` / `Non-Weighted` * 1000,
+    Items_1000_Weighted_LS = `Claim PD Number of Paid Items` / Weighted * 1000,
+    GIC_1000_LS = `Claim PD Paid GIC excl. BB` / `Non-Weighted` * 1000,
+    GIC_1000_Weighted_LS = `Claim PD Paid GIC excl. BB` / Weighted * 1000,
+    CPI_1000_LS = `Cost per item` / `Non-Weighted` * 1000,
+    CPI_1000_Weighted_LS = `Cost per item` / Weighted * 1000
+  )
+
+plot <- plot_ly(
+  data = list_sizes,
+  x = ~`Paid Date`,
+  y = ~Items_1000_LS,
+  color = ~`Presc Health Board Name`,
+  type = 'scatter',
+  mode = 'lines'
+)
+
+plot <- plot_ly(
+  data = list_sizes %>% filter(!(`Presc Health Board Name` == "SCOTLAND")),
+  x = ~`Paid Date`,
+  y = ~Items_1000_Weighted_LS,
+  color = ~`Presc Health Board Name`,
+  type = 'scatter',
+  mode = 'lines'
+)
+
+# Add a bold version of one specific line (e.g., 'NHS Greater Glasgow & Clyde')
+plot <- plot %>%
+  add_trace(
+    data = subset(list_sizes, `Presc Health Board Name` == "SCOTLAND"),
+    x = ~`Paid Date`,
+    y = ~Items_1000_Weighted_LS,
+    type = 'scatter',
+    mode = 'lines',
+    line = list(width = 4,
+                color = "#3F3685"),      # ← Bold effect
+    name = "SCOTLAND"
+  )
+
+# Monthly trend extraction for Scotland
+timeseries_items_trend <- forecast_items %>%
+  filter(Board == "SCOTLAND") %>%
+  arrange(Date) %>%
+  mutate(
+    month = months(Date),
+    month = factor(month, levels = c(
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ))
+  ) %>%
+  filter(!is.na(Measure)) %>%
+  group_by(month) %>%
+  summarise(Average = mean(Measure), .groups = "drop") %>%
+  arrange(month)
+
+seasonality_items_chart <- ggplot(
+  timeseries_items_trend,
+  aes(x = month, y = Average)) +
+  geom_line(group = 1, color = phs_colours("phs-purple"), linewidth = 1.5) +
+  #geom_point() +
+  scale_y_continuous(labels = comma) +
+  labs(title = "Seasonality chart showing mean number of paid items in Scotland across each month from 2010 to 2025",
+       x = "Month",
+       y = "Mean number of paid items") +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+timeseries_gic_trend <- forecast_gic %>%
+  filter(Board == "SCOTLAND",
+         Date < "2026-01-01") %>%
+  arrange(Date) %>%
+  mutate(
+    month = months(Date),
+    month = factor(month, levels = c(
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ))
+  ) %>%
+  #filter(!is.na(Measure)) %>%
+  group_by(month) %>%
+  summarise(Average = mean(Measure), .groups = "drop") %>%
+  arrange(month)
+
+seasonality_gic_chart <- ggplot(
+  timeseries_gic_trend,
+  aes(x = month, y = Average)) +
+  geom_line(group = 1, color = phs_colours("phs-purple"), linewidth = 1.5) +
+  #geom_point() +
+  scale_y_continuous(labels = comma) +
+  labs(title = "Seasonality chart showing mean Gross Ingredient Cost (£) in Scotland across each month from 2010 to 2025",
+       x = "Month",
+       y = "Mean Gross Ingredient Cost (£)") +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+correlation_items_trend <- forecast_items %>%
+  filter(Board == "SCOTLAND") %>%
+  arrange(Date) %>%
+  filter(Date < "2026-01-01") %>%
+  select(Date, Board, `Number of Paid Items` = Measure)
+
+correlation_gic_trend <- forecast_gic %>%
+  filter(Board == "SCOTLAND") %>%
+  arrange(Date) %>%
+  filter(Date < "2026-01-01") %>%
+  select(Date, Board, `Gross Ingredient Cost (£)` = Measure)
+
+correlation <- left_join(correlation_items_trend, correlation_gic_trend)
+
+correlation_plot <- correlation %>%
+  ggpairs(columns = 3:4,
+          lower = list(continuous = wrap("points", colour = phs_colours("phs-purple"))),
+          diag  = list(continuous = wrap("densityDiag", fill = phs_colours("phs-purple"), colour = phs_colours("phs-purple"))),
+          upper = list(continuous = wrap("cor", colour = phs_colours("phs-purple")))
+  ) +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 14),
+    strip.text = element_text(size = 14)  # variable names on panels
+  )
+
+
+# Define a scaling factor
+scale_factor <- max(correlation$`Number of Paid Items`, na.rm = TRUE) / 
+  max(correlation$`Gross Ingredient Cost (£)`, na.rm = TRUE)
+
+# pivot_corr <- correlation %>%
+#   pivot_longer(cols = 3:4,
+#                names_to = "Variable",
+#                values_to = "Value")
+
+ggplot(correlation, aes(x = Date, y = `Number of Paid Items`)) +
+  geom_line(colour = phs_colours("phs-purple"), size = 1) +
+  labs(title = "Monthly trend chart showing number of paid items in Scotland from 2010 to 2025",
+       x = "Month",
+       y = "Number of Paid Items") +
+  scale_y_continuous(labels = comma) +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+ggplot(correlation, aes(x = Date, y = `Gross Ingredient Cost (£)`)) +
+  geom_line(colour = phs_colours("phs-purple"), size = 1) +
+  labs(title = "Monthly trend chart showing Gross Ingredient Cost (£) in Scotland from 2010 to 2025",
+       x = "Month",
+       y = "Gross Ingredient Cost (£)") +
+  scale_y_continuous(labels = comma) +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+cpi <- forecast_cpi %>%
+  filter(Board == "SCOTLAND",
+         Date > "2011-03-31" & Date < "2026-01-01") %>%
+  select(Date, Measure) %>%
+  arrange(Date)
+
+ggplot(cpi,
+       aes(x = Date, y = Measure)) +
+  geom_line(colour = phs_colours("phs-purple"), size = 1) +
+  labs(title = "Monthly trend chart showing average cost per item in Scotland from 2010 to 2025",
+       x = "Month",
+       y = "Average cost per item") +
+  scale_y_continuous(labels = comma) +
+  theme(plot.title = element_text(size = 18),
+        axis.title.x = element_text(size = 14), # X-axis title
+        axis.title.y = element_text(size = 14), # Y-axis title
+        axis.text.x = element_text(size = 12), # X-axis labels
+        axis.text.y = element_text(size = 12)) # Y-axis labels
+
+acf_pacf_items <- forecast_items %>%
+  filter(Board == "NHS AYRSHIRE & ARRAN") %>%
+  arrange(Date) %>%
+  filter(Date > "2011-01-01" & Date < "2026-01-01") %>%
+  select(Date, `Number of Paid Items` = Measure) %>%
+  mutate(Month = yearmonth(Date)) %>%
+  as_tsibble(index = Month) %>%
+  mutate(
+    diff_1 = difference(`Number of Paid Items`, 1),
+    diff_12 = difference(diff_1, 12)
+  ) %>%
+  gg_tsdisplay(diff_12)
+
+
+autoplot(acf_pacf_items, `Number of Paid Items`)
+
+acf_pacf_items %>%
+  ACF(`Number of Paid Items`) %>%
+  autoplot()
+
+acf_pacf_items %>%
+  gg_tsdisplay(difference(`Number of Paid Items`, 12),
+               plot_type = 'partial', lag = 36) +
+  labs(title="Seasonally differenced", y="")
+
+test_mape <- rbind(`forecast-performance-2026-05-28` %>% select(board, type, MAPE, run), forecast_performance %>% select(board, type, MAPE, run))
+
+t1 <- test_mape %>%
+  filter(!board == "SCOTLAND") %>%
+  group_by(run, type) %>%
+  summarise(avg = mean(MAPE))
+
+##### Read in machine learning model results
+ml_preds <- read_excel("model_outputs.xlsx", sheet = "Predictions") %>%
+  mutate(time = ymd(time))
+
+ml_preds_new <- ml_preds %>%
+  select(Board, Date = time, Type = Target, Measure = Actual, Predicted, Model) %>%
+  pivot_wider(names_from = Model, values_from = Predicted)
+
+forecast_comp <- readRDS(paste0('shiny/forecasts/sarima/output/forecast-run-999.rds')) %>%
+  filter(Historical_Data == "12 months of historical data",
+         Year == 2025,
+         F_Horizon == 48,
+         Arima_Error == FALSE) %>%
+  select(-c(Historical_Data, Year, F_Horizon, Arima_Error))
+
+model_comp <- forecast_comp %>%
+  select(Board, Date, Type, Measure, SARIMA = Forecast) %>%
+  filter(Type %in% c("Claim PD Number of Paid Items", "Claim PD GIC excl. BB")) %>%
+  left_join(ml_preds_new)
+
+
 
 ## Turn dataframe from setup into tsibble
 eda_data_tsibble <- reactive({
--- shiny/setup.R
+++ shiny/setup.R
@@ -40,8 +40,6 @@ library(phsmethods)
 library(tibble)
 library(tsibble)
 library(feasts)
-library(GGally)
-library(scales)
 
 # PHS styling packages ----
 library(phsstyles)
@@ -94,7 +92,7 @@ forecast_performance_date = '2026-05-24' # in the format YYYY-MM-DD
 # LOAD IN DATA HERE ----
 
 ############## Forecast ############## 
-forecast <- readRDS(paste0('shiny/forecasts/sarima/output/forecast-run-', run_number,'.rds')) %>%
+forecast <- readRDS(paste0('shiny/forecasts/sarima/output/run ', run_number, '/forecast-run-', run_number,'.rds')) %>%
   filter(Historical_Data == "12 months of historical data",
          Year == latest_year,
          F_Horizon == 48,
@@ -184,15 +182,15 @@ forecast_cpi <- forecast %>%
 #   mutate(Type = "Phasings")
 # 
 # ### Save file for 26/27
-# write.xlsx(forecast_phasings, 'shiny/data/2627 Phasings.xlsx')
+# write.xlsx(forecast_phasings, 'shiny/data/Phasings/2627 Phasings.xlsx')
 
 ### Phasings - leave uncommented once new file has been saved
-forecast_phasings <- read_excel('shiny/data/2627 Phasings.xlsx')
+forecast_phasings <- read_excel('shiny/data/Phasings/2627 Phasings.xlsx')
 
 ############## Trend Monitoring Data ############## 
 
 ## List all Excel files in the directory
-file_list <- list.files(path = "shiny/data/Items per prescription/", pattern = "\\.xlsx$", full.names = TRUE)
+file_list <- list.files(path = "shiny/data/Supplementary Trend Data/", pattern = "\\.xlsx$", full.names = TRUE)
 
 ## Read all files into a list of data frames
 data_list <- map(file_list, ~read.xlsx(.x, startRow = 2, cols = 2:7, sep.names = ' '))
@@ -220,7 +218,7 @@ combined_data <- combined_data %>%
 ## compare figures with working days
 combined_data_wd <- combined_data
 
-business_days <- readRDS('shiny/forecasts/data/business_days_lookup.rds') %>%
+business_days <- readRDS('shiny/data/Business Days/business_days_lookup.rds') %>%
   dplyr::rename(`Paid Date` = Date)
 
 combined_data_wd <- combined_data_wd %>%
@@ -232,274 +230,6 @@ combined_data_wd <- combined_data_wd %>%
 ############## Performance ############## 
 forecast_performance <- read_excel('shiny/forecasts/sarima/output/forecast-performance.xlsx')
 
-############## EDA ############## 
-eda_data <- read.csv('shiny/forecasts/data/Historical Data.csv', check.names = FALSE) %>%
-  mutate(`Paid Date` = dmy(`Paid Date`)) %>%
-  filter(`Paid Date` > '2009-12-31') 
-
-eda_data$`Claim PD Paid GIC excl. BB` <- as.double(gsub(",", "", eda_data$`Claim PD Paid GIC excl. BB`))
-
-scotland_data <- eda_data %>%
-  group_by(`Paid Date`) %>%
-  summarise(across(where(is.numeric), ~sum(.x, na.rm = TRUE))) %>%
-  mutate(`Presc Health Board Name` = 'SCOTLAND') %>%
-  select(`Presc Health Board Name`, everything())
-
-eda_data <- eda_data %>%
-  bind_rows(scotland_data) %>%
-  mutate(`Cost per item` = `Claim PD Paid GIC excl. BB` / `Claim PD Number of Paid Items`) %>%
-  # double check data is arranged by board and date 
-  arrange(`Presc Health Board Name`, `Paid Date`)
-
-rm(scotland_data)
-
-# eda_data_tsibble <- eda_data %>%
-#   # create new column for index
-#   mutate(month_new = yearmonth(`Paid Date`)) %>%
-#   tsibble(index = 'month_new')
-
-############## per 1,000 list size (weighted and non-weighted) ############## 
-list_sizes <- read.xlsx('shiny/data/population/List Sizes.xlsx') %>%
-  mutate(quarter_date = as.Date(quarter_date, origin = "1899-12-30")) %>%
-  dplyr::rename(`Paid Date` = quarter_date,
-                `Presc Health Board Name` = Board) %>%
-  select(`Presc Health Board Name`, everything()) %>%
-  left_join(eda_data, by = c('Presc Health Board Name', 'Paid Date')) %>%
-  select(`Presc Health Board Name`, `Paid Date`, `Claim PD Number of Paid Items`, `Claim PD Paid GIC excl. BB`, `Cost per item`, everything()) %>%
-  mutate(
-    Items_1000_LS = `Claim PD Number of Paid Items` / `Non-Weighted` * 1000,
-    Items_1000_Weighted_LS = `Claim PD Number of Paid Items` / Weighted * 1000,
-    GIC_1000_LS = `Claim PD Paid GIC excl. BB` / `Non-Weighted` * 1000,
-    GIC_1000_Weighted_LS = `Claim PD Paid GIC excl. BB` / Weighted * 1000,
-    CPI_1000_LS = `Cost per item` / `Non-Weighted` * 1000,
-    CPI_1000_Weighted_LS = `Cost per item` / Weighted * 1000
-  )
-
-plot <- plot_ly(
-  data = list_sizes,
-  x = ~`Paid Date`,
-  y = ~Items_1000_LS,
-  color = ~`Presc Health Board Name`,
-  type = 'scatter',
-  mode = 'lines'
-)
-
-plot <- plot_ly(
-  data = list_sizes %>% filter(!(`Presc Health Board Name` == "SCOTLAND")),
-  x = ~`Paid Date`,
-  y = ~Items_1000_Weighted_LS,
-  color = ~`Presc Health Board Name`,
-  type = 'scatter',
-  mode = 'lines'
-)
-
-# Add a bold version of one specific line (e.g., 'NHS Greater Glasgow & Clyde')
-plot <- plot %>%
-  add_trace(
-    data = subset(list_sizes, `Presc Health Board Name` == "SCOTLAND"),
-    x = ~`Paid Date`,
-    y = ~Items_1000_Weighted_LS,
-    type = 'scatter',
-    mode = 'lines',
-    line = list(width = 4,
-                color = "#3F3685"),      # ← Bold effect
-    name = "SCOTLAND"
-  )
-
-# Monthly trend extraction for Scotland
-timeseries_items_trend <- forecast_items %>%
-  filter(Board == "SCOTLAND") %>%
-  arrange(Date) %>%
-  mutate(
-    month = months(Date),
-    month = factor(month, levels = c(
-      "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ))
-  ) %>%
-  filter(!is.na(Measure)) %>%
-  group_by(month) %>%
-  summarise(Average = mean(Measure), .groups = "drop") %>%
-  arrange(month)
-
-seasonality_items_chart <- ggplot(
-  timeseries_items_trend,
-  aes(x = month, y = Average)) +
-  geom_line(group = 1, color = phs_colours("phs-purple"), linewidth = 1.5) +
-  #geom_point() +
-  scale_y_continuous(labels = comma) +
-  labs(title = "Seasonality chart showing mean number of paid items in Scotland across each month from 2010 to 2025",
-       x = "Month",
-       y = "Mean number of paid items") +
-  theme(plot.title = element_text(size = 18),
-        axis.title.x = element_text(size = 14), # X-axis title
-        axis.title.y = element_text(size = 14), # Y-axis title
-        axis.text.x = element_text(size = 12), # X-axis labels
-        axis.text.y = element_text(size = 12)) # Y-axis labels
-
-timeseries_gic_trend <- forecast_gic %>%
-  filter(Board == "SCOTLAND",
-         Date < "2026-01-01") %>%
-  arrange(Date) %>%
-  mutate(
-    month = months(Date),
-    month = factor(month, levels = c(
-      "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ))
-  ) %>%
-  #filter(!is.na(Measure)) %>%
-  group_by(month) %>%
-  summarise(Average = mean(Measure), .groups = "drop") %>%
-  arrange(month)
-
-seasonality_gic_chart <- ggplot(
-  timeseries_gic_trend,
-  aes(x = month, y = Average)) +
-  geom_line(group = 1, color = phs_colours("phs-purple"), linewidth = 1.5) +
-  #geom_point() +
-  scale_y_continuous(labels = comma) +
-  labs(title = "Seasonality chart showing mean Gross Ingredient Cost (£) in Scotland across each month from 2010 to 2025",
-       x = "Month",
-       y = "Mean Gross Ingredient Cost (£)") +
-  theme(plot.title = element_text(size = 18),
-        axis.title.x = element_text(size = 14), # X-axis title
-        axis.title.y = element_text(size = 14), # Y-axis title
-        axis.text.x = element_text(size = 12), # X-axis labels
-        axis.text.y = element_text(size = 12)) # Y-axis labels
-
-correlation_items_trend <- forecast_items %>%
-  filter(Board == "SCOTLAND") %>%
-  arrange(Date) %>%
-  filter(Date < "2026-01-01") %>%
-  select(Date, Board, `Number of Paid Items` = Measure)
-
-correlation_gic_trend <- forecast_gic %>%
-  filter(Board == "SCOTLAND") %>%
-  arrange(Date) %>%
-  filter(Date < "2026-01-01") %>%
-  select(Date, Board, `Gross Ingredient Cost (£)` = Measure)
-
-correlation <- left_join(correlation_items_trend, correlation_gic_trend)
-
-correlation_plot <- correlation %>%
-  ggpairs(columns = 3:4,
-          lower = list(continuous = wrap("points", colour = phs_colours("phs-purple"))),
-          diag  = list(continuous = wrap("densityDiag", fill = phs_colours("phs-purple"), colour = phs_colours("phs-purple"))),
-          upper = list(continuous = wrap("cor", colour = phs_colours("phs-purple")))
-  ) +
-  theme(
-    axis.text = element_text(size = 12),
-    axis.title = element_text(size = 14),
-    strip.text = element_text(size = 14)  # variable names on panels
-  )
-
-
-# Define a scaling factor
-scale_factor <- max(correlation$`Number of Paid Items`, na.rm = TRUE) / 
-  max(correlation$`Gross Ingredient Cost (£)`, na.rm = TRUE)
-
-# pivot_corr <- correlation %>%
-#   pivot_longer(cols = 3:4,
-#                names_to = "Variable",
-#                values_to = "Value")
-
-ggplot(correlation, aes(x = Date, y = `Number of Paid Items`)) +
-  geom_line(colour = phs_colours("phs-purple"), size = 1) +
-  labs(title = "Monthly trend chart showing number of paid items in Scotland from 2010 to 2025",
-       x = "Month",
-       y = "Number of Paid Items") +
-  scale_y_continuous(labels = comma) +
-  theme(plot.title = element_text(size = 18),
-        axis.title.x = element_text(size = 14), # X-axis title
-        axis.title.y = element_text(size = 14), # Y-axis title
-        axis.text.x = element_text(size = 12), # X-axis labels
-        axis.text.y = element_text(size = 12)) # Y-axis labels
-  
-ggplot(correlation, aes(x = Date, y = `Gross Ingredient Cost (£)`)) +
-  geom_line(colour = phs_colours("phs-purple"), size = 1) +
-  labs(title = "Monthly trend chart showing Gross Ingredient Cost (£) in Scotland from 2010 to 2025",
-       x = "Month",
-       y = "Gross Ingredient Cost (£)") +
-  scale_y_continuous(labels = comma) +
-  theme(plot.title = element_text(size = 18),
-        axis.title.x = element_text(size = 14), # X-axis title
-        axis.title.y = element_text(size = 14), # Y-axis title
-        axis.text.x = element_text(size = 12), # X-axis labels
-        axis.text.y = element_text(size = 12)) # Y-axis labels
-
-cpi <- forecast_cpi %>%
-  filter(Board == "SCOTLAND",
-         Date > "2011-03-31" & Date < "2026-01-01") %>%
-  select(Date, Measure) %>%
-  arrange(Date)
-
-ggplot(cpi,
-       aes(x = Date, y = Measure)) +
-  geom_line(colour = phs_colours("phs-purple"), size = 1) +
-  labs(title = "Monthly trend chart showing average cost per item in Scotland from 2010 to 2025",
-       x = "Month",
-       y = "Average cost per item") +
-  scale_y_continuous(labels = comma) +
-  theme(plot.title = element_text(size = 18),
-        axis.title.x = element_text(size = 14), # X-axis title
-        axis.title.y = element_text(size = 14), # Y-axis title
-        axis.text.x = element_text(size = 12), # X-axis labels
-        axis.text.y = element_text(size = 12)) # Y-axis labels
-
-acf_pacf_items <- forecast_items %>%
-  filter(Board == "NHS AYRSHIRE & ARRAN") %>%
-  arrange(Date) %>%
-  filter(Date > "2011-01-01" & Date < "2026-01-01") %>%
-  select(Date, `Number of Paid Items` = Measure) %>%
-  mutate(Month = yearmonth(Date)) %>%
-  as_tsibble(index = Month) %>%
-  mutate(
-    diff_1 = difference(`Number of Paid Items`, 1),
-    diff_12 = difference(diff_1, 12)
-  ) %>%
-  gg_tsdisplay(diff_12)
-
-
-autoplot(acf_pacf_items, `Number of Paid Items`)
-
-acf_pacf_items %>%
-  ACF(`Number of Paid Items`) %>%
-  autoplot()
-
-acf_pacf_items %>%
-  gg_tsdisplay(difference(`Number of Paid Items`, 12),
-               plot_type = 'partial', lag = 36) +
-  labs(title="Seasonally differenced", y="")
-
-test_mape <- rbind(`forecast-performance-2026-05-28` %>% select(board, type, MAPE, run), forecast_performance %>% select(board, type, MAPE, run))
-
-t1 <- test_mape %>%
-  filter(!board == "SCOTLAND") %>%
-  group_by(run, type) %>%
-  summarise(avg = mean(MAPE))
-
-##### Read in machine learning model results
-ml_preds <- read_excel("model_outputs.xlsx", sheet = "Predictions") %>%
-  mutate(time = ymd(time))
-
-ml_preds_new <- ml_preds %>%
-  select(Board, Date = time, Type = Target, Measure = Actual, Predicted, Model) %>%
-  pivot_wider(names_from = Model, values_from = Predicted)
-
-forecast_comp <- readRDS(paste0('shiny/forecasts/sarima/output/forecast-run-999.rds')) %>%
-  filter(Historical_Data == "12 months of historical data",
-         Year == 2025,
-         F_Horizon == 48,
-         Arima_Error == FALSE) %>%
-  select(-c(Historical_Data, Year, F_Horizon, Arima_Error))
-
-model_comp <- forecast_comp %>%
-  select(Board, Date, Type, Measure, SARIMA = Forecast) %>%
-  filter(Type %in% c("Claim PD Number of Paid Items", "Claim PD GIC excl. BB")) %>%
-  left_join(ml_preds_new)
-  
-
 ############## Variables ############## 
 healthboards <- unique(forecast_items$Board)
 financial_years <- unique(forecast_items$FY)
@@ -510,7 +240,6 @@ my_theme <- bs_theme(
   primary = "#3F3685",        # Change primary button colour
   secondary = "#0078D4"       # Change secondary button colour
 )
-
 
 
 
